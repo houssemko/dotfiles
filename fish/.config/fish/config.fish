9ac1d0aa526cbe8d1b7c934855d6b278d85b7078
houssem <houssem@users.noreply.github.com>
2026-08-18 12:24:42 +0100

    update configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -162,9 +162,3 @@ set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 
 # Added by Antigravity CLI installer
 set -gx PATH "/home/houssem/.local/bin" $PATH
-# test
-# test
-# bg test 2
-# test3
-test
-test
