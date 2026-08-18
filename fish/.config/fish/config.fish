20ec162da05d4f1cd5db81758155b954c811b905
houssemko <41808005+houssemko@users.noreply.github.com>
2026-08-18 14:19:21 +0100

    Remove PATH and API key from config.fish
    Removed Antigravity CLI installer modifications.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -181,7 +181,3 @@ end
 # Use fd as the default fzf command
 set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 
-# Added by Antigravity CLI installer
-set -gx PATH "/home/houssem/.local/bin" $PATH
-
-export DASHSCOPE_API_KEY="***REMOVED***"
