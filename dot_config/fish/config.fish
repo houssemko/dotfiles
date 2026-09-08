89bf5e8507a8f43d24481635f691ac681f499042
houssem <houssem@local>
2026-09-08 20:35:32 +0100

    Update .config/fish/config.fish Update .config/mpv/mpv.conf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,7 +5,6 @@ abbr wt curl wttr.in/Monastir
 abbr ve pacman -Qs
 abbr ff fastfetch
 abbr se paru -Ss
-
 abbr in sudo pacman -Sy
 abbr ins paru -Sy --noconfirm
 abbr rem sudo pacman -Runs
@@ -14,12 +13,11 @@ abbr bl sudo systemctl restart bluetooth
 abbr mic sudo micro
 abbr mi micro
 abbr e exit
-abbr p s-tui
-abbr merge ffmpeg -f concat -i file.txt -c copy merged.mp4
-abbr z zoxide 
-abbr gc git clone 
 abbr fi flatpak install
 abbr fu flatpak uninstall
 
 # Use fd as the default fzf command
 set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+
+# ante
+fish_add_path /home/houssem/.ante/bin
